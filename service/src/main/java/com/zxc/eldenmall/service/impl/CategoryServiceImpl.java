2e7a1a3f33bda9d6146a04a6a5322fff5b91f48d
wahaha <wahahahage@gmail.com>
2022-10-07 02:27:21 +0800

    首页商品推荐和商品分类添加缓存功能

--- service/src/main/java/com/zxc/eldenmall/service/impl/CategoryServiceImpl.java
+++ service/src/main/java/com/zxc/eldenmall/service/impl/CategoryServiceImpl.java
@@ -1,14 +1,20 @@
 package com.zxc.eldenmall.service.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zxc.eldenmall.dao.CategoryMapper;
 import com.zxc.eldenmall.entity.CategoryVO;
 import com.zxc.eldenmall.service.CategoryService;
 import com.zxc.eldenmall.vo.ResStatus;
 import com.zxc.eldenmall.vo.ResultVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author wahaha
@@ -19,12 +25,32 @@ public class CategoryServiceImpl implements CategoryService {
     @Autowired
     private CategoryMapper categoryMapper;
 
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
 
     @Override
     public ResultVO listCategories() {
-        List<CategoryVO> categoryVos = categoryMapper.selectAllCategories();
-
-        return new ResultVO(ResStatus.OK, "success", categoryVos);
+        List<CategoryVO> categoryVos = null;
+        try {
+            //从redis中进行查询
+            String categories = redisTemplate.boundValueOps("categories").get();
+            //查到则直接转换格式输出
+            if(categories !=null){
+                JavaType type = objectMapper.getTypeFactory().constructParametricType(ArrayList.class, CategoryVO.class);
+                categoryVos = objectMapper.readValue(categories,type);
+            } else {
+                //没查到则到数据库中查找
+                categoryVos = categoryMapper.selectAllCategories();
+                String str = objectMapper.writeValueAsString(categoryVos);
+                redisTemplate.boundValueOps("categories").set(str,1, TimeUnit.DAYS);
+            }
+        } catch (JsonProcessingException e){
+            e.printStackTrace();
+        }
+        return new ResultVO(ResStatus.OK,"success",categoryVos);
     }
 
     @Override
--- service/src/main/java/com/zxc/eldenmall/service/impl/IndexImgServiceImpl.java
+++ service/src/main/java/com/zxc/eldenmall/service/impl/IndexImgServiceImpl.java
@@ -1,13 +1,19 @@
 package com.zxc.eldenmall.service.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zxc.eldenmall.dao.IndexImgMapper;
 import com.zxc.eldenmall.entity.IndexImg;
 import com.zxc.eldenmall.service.IndexImgService;
 import com.zxc.eldenmall.vo.ResStatus;
 import com.zxc.eldenmall.vo.ResultVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -19,13 +25,33 @@ public class IndexImgServiceImpl implements IndexImgService {
     @Autowired
     private IndexImgMapper indexImgMapper;
 
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public ResultVO listIndexImgs() {
-        List<IndexImg> indexImgs = indexImgMapper.listIndexImgs();
-        if(indexImgs.size() == 0){
-            return new ResultVO(ResStatus.NO,"寄了",null);
+        List<IndexImg> indexImgs = null;
+        try {
+            //从redis中取id对应的数据
+            String imgStr = redisTemplate.boundValueOps("indexImgs").get();
+            //如果redis中存在数据，则取出json数据进行格式转换
+            if(imgStr != null){
+                JavaType type = objectMapper.getTypeFactory().constructParametricType(ArrayList.class, IndexImg.class);
+                indexImgs = objectMapper.readValue(imgStr, type);
+            } else {
+                //如果不存在则去数据库中查询
+                indexImgs = indexImgMapper.listIndexImgs();
+                redisTemplate.boundValueOps("indexImgs").set(objectMapper.writeValueAsString(indexImgs));
+            }
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        if(indexImgs != null){
+            return new ResultVO(ResStatus.OK, "success",indexImgs);
         } else {
-            return new ResultVO(ResStatus.OK,"success",indexImgs);
+            return new ResultVO(ResStatus.NO, "寄", null);
         }
     }
 }
